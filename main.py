f60c686cda5a28121c59694077090eff84800a20
Mojiwx <mojtaba.shd94@gmail.com>
2026-06-22 17:39:20 +0330

    feat: support multiple product tracking

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import json
 from scraper import get_price
 from utils import update_price
+
+
+def load_products():
+    with open("products.json", "r") as f:
+        return json.load(f)
 
 
 def clean_price(str_price):
@@ -11,24 +17,20 @@ def clean_price(str_price):
                )
 
 
-product_name = "iphone_17_pro_max_256_12_CH"
+products = load_products()
 
-product_url = (
-    "https://www.digikala.com/product/dkp-20481189/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84"
-    "-%D8%A7%D9%BE%D9%84-%D9%85%D8%AF%D9%84-iphone-17-pro-max-ch-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8"
-    "%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-256-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1"
-    "%D9%85-12-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%86%D8%A7%D8%AA-%D8%A7%DA%A9%D8%AA%DB%8C%D9%88/")
+for product_name, product_url in products.items():
+    print(f"\nChecking: {product_name}")
 
-raw_price = get_price(product_url)
+    raw_price = get_price(product_url)
 
-try:
-    if raw_price:
-        price = clean_price(raw_price)
-        print(product_name)
-        print(f"Current Price: {price}")
+    try:
+        if raw_price:
+            price = clean_price(raw_price)
+            print(f"Current Price: {price}")
 
-        update_price(product_name, product_url, price)
-    else:
-        print("Price not find")
-except Exception as e:
-    print("Error: ", e)
+            update_price(product_name, product_url, price)
+        else:
+            print("Price not find")
+    except Exception as e:
+        print("Error: ", e)
